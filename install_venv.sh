adc0396f19d3b7b4530da7bfe42e5eb1009f854a
Abhishek-Kuvalekar <abhi.kuvalekar@gmail.com>
2017-12-21 19:46:00 +0530

    Added installation commands for linux.

---
+++ install_venv.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+python3 -m venv flask
+source flask/bin/activate
+pip install flask
+pip install flask-login
+pip install flask-openid
+pip install flask-mail
+pip install flask-sqlalchemy
+pip install sqlalchemy-migrate
+pip install flask-whooshalchemy
+pip install flask-wtf
+pip install flask-babel
+pip install guess_language
+pip install flipflop
+pip install coverage
